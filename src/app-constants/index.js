0459ed64f71c0ef329e66addc5a153870a7ad9e8
kdotamoh <i.be.your.strychnine@gmail.com>
2020-03-15 19:23:26 +0000

    root font size mobile

--- src/app-constants/index.js
+++ src/app-constants/index.js
@@ -7,8 +7,13 @@ export const COLORS = {
 };
 
 export const H1_SIZE = 5.8;
+// Major Third type scale (1.25)
 export const H2_SIZE = H1_SIZE / 1.25;
 export const H3_SIZE = H2_SIZE / 1.25;
 export const H4_SIZE = H3_SIZE / 1.25;
 export const H5_SIZE = H4_SIZE / 1.25;
 export const H6_SIZE = H5_SIZE / 1.25;
+
+export const H1_SIZE_SM = 2.4;
+export const H2_SIZE_SM = H2_SIZE / 1.125;
+export const H3_SIZE_SM = H3_SIZE / 1.125;
