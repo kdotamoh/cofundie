a739cea9b1f13f54ff6ded2c321ef26cd37c208a
kdotamoh <i.be.your.strychnine@gmail.com>
2020-03-17 22:06:29 +0000

    button styles

--- src/components/styled.js
+++ src/components/styled.js
@@ -7,6 +7,7 @@ export const Button = styled.button`
   ${layout}
   ${space}
   ${color}
+  
 
   background-color: ${props => props.bg};
   border-color: ${props => (props.borderColor ? props.borderColor : props.bg)};
@@ -14,6 +15,12 @@ export const Button = styled.button`
   color: ${props => props.color};
   font-size: 2.4rem;
   padding: 1.6rem 6rem;
+  display: inline-block;
+  border: none;
+  text-decoration: none;
+  cursor: pointer;
+  text-align: center;
+  transition: background 250ms ease-in-out, transform 150ms ease;
 
   @media (max-width: 768px) {
   padding: 1.2rem 3rem;
